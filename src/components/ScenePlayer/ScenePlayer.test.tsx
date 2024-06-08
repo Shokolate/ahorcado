bb21657167f0fe0e86722f50ff1e3714b1d6bfb6
Shokolate <55993657+Shokolate@users.noreply.github.com>
2024-06-08 00:48:03 -0300

    Le agregué comentarios =P

--- src/components/ScenePlayer/ScenePlayer.test.tsx
+++ src/components/ScenePlayer/ScenePlayer.test.tsx
@@ -5,16 +5,15 @@ import ScenePlayer from ".";
 describe("ScenePlayer", () => {
     it("ScenePlayer should show a message when the tries reach 0", () => {
         render(
+            //Set tries to 0
             <ScenePlayer
                 tries={0}
             />
         );
-
+        //Creates a constant to capture the state of "h2-ScenePlayer"
         const msg_h2 = screen.getByTestId("h2-ScenePlayer");
 
-        // no se si necesito esto
-        screen.debug();
-
+        //Defines the expected message
         expect(msg_h2.textContent).toBe("You are dead");
     });
 });
